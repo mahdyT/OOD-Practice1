14dac6ea6a5c53ffd67be1d4ee711d62ce79a4c9
mahdyT <mahdy.taghiloo.83@gmail.com>
2025-12-12 23:43:19 +0330

    add On-site payment

--- OOD-without-step1/services/PaymentProcessor.java
+++ OOD-without-step1/services/PaymentProcessor.java
@@ -4,4 +4,5 @@ class PaymentProcessor {
     public void payByCard(double amount){ System.out.println("Paid by card: " + amount); }
     public void payByCash(double amount){ System.out.println("Paid by cash: " + amount); }
     public void payByPayPal(double amount){ System.out.println("Paid by PayPal: " + amount); }
+    public void payByOnsite(double amount){ System.out.println("Paid On-site: " + amount); }
 }
